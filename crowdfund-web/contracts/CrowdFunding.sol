0845765c81aaa50d01b4d8f04bf97f8264fa4f2b
curious <vishal238128@gmail.com>
2023-01-29 11:33:42 +0530

    code added in solidity file

--- crowdfund-web/contracts/CrowdFunding.sol
+++ crowdfund-web/contracts/CrowdFunding.sol
@@ -19,10 +19,37 @@ contract CrowdFunding {
     uint256 public numberOfCampaigns = 0;
 
     function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
-        
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(campaign.deadline < block.timestamp, "The deadline date should be in the future");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+
     }
 
-    function donateToCampaign() {}
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent) = payable(campaign.owner).call{value: amount}("");
+
+        
+
+    }
 
     function getDonators() {}
 
