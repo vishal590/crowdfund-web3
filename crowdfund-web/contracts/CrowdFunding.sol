3b6369b3e1d10d87941dab212c7dcf1b1fa884ff
curious <vishal238128@gmail.com>
2023-01-30 19:16:07 +0530

    code added in client

--- crowdfund-web/contracts/CrowdFunding.sol
+++ crowdfund-web/contracts/CrowdFunding.sol
@@ -45,15 +45,29 @@ contract CrowdFunding {
         campaign.donators.push(msg.sender);
         campaign.donations.push(amount);
 
-        (bool sent) = payable(campaign.owner).call{value: amount}("");
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
 
-        
+        if(sent){
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
 
     }
 
-    function getDonators() {}
+    function getDonators(uint256 _id) view public returns(address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
 
-    function getCampaign() {}
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for(uint i = 0; i < numberOfCampaigns; i++){
+            Campaign storage item = campaigns[i];
+
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
+    }
 
 
 
